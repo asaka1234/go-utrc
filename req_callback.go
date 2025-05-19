d2c4fe85b94c88efab077c563aa61dd42b7f526d
cyrildou <cyrildou@live.cn>
2025-05-19 22:56:13 +0800

    1st version

--- req_callback.go
+++ req_callback.go
@@ -1,7 +1,7 @@
 package go_utrc
 
 // 充值回调
-func (cli *Client) CashierCallback(req UTRCDepositCompleteBackReq, processor func(UTRCDepositCompleteBackReq) error) error {
+func (cli *Client) DepositCallback(req UTRCDepositCompleteBackReq, processor func(UTRCDepositCompleteBackReq) error) error {
 	//验证签名
 	//TODO
 
